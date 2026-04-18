6fd16819a14355310bb8b8116a22c83c1faa8296
Shahu1232 <subhashshahu16@gmail.com>
2026-04-18 09:29:15 -0400

    Add files via upload

---
+++ python assigment.py
@@ -0,0 +1,56 @@
+"""
+Q = 1.write a program to print "Hello python" on the screen.
+ Ans= print("Hello python")
+
+ 2.write a program to take your name as input and print "Hello <name>
+
+ Ans= name= input("Hello",name)
+   print(name)
+
+3. write a program to take one integer input from the user and print it
+
+   Ans= number1= int(input("Enter the number: ") )
+
+4. write a program to take two integers as input and print their sum
+
+  Ans= number1 = int(input("Enter the number1 value: ") )
+       number2= int(input("enter the number 2 value: ") )
+       number3 =number1+number2
+       print(Total is:",number3)
+
+5.write a program to take two integers as input and print their multiplication
+
+   ans= a=int(input("enter the first number: ") )
+        b=int(input("enter the second number: ") )
+        c= a*b
+        print("multiplication result is: " ,c)
+
+6. write a program to take one float number as input and print it.
+
+  ans= f=float(input("enter the decimal value: ") )
+      print("float number is:" 'f)
+
+
+7.write a program to take two float numbers as input and print their average
+   ans= f1=float(input("enter first float number:") )
+       f2=float(input("enter second float number:") )
+       f3=(f1+f2)/2
+       prnt("Average float number is :"f3)
+
+8.write a program to take length and width (float) from the user and calculate the area of a rectangle.
+  ans= len=float(input("enter the area of length: " ) )
+       width=float(input("enter the area of width: ") )
+       area=len * width
+       print("total area is : ",area)
+
+9.write a program to take an integer input and convert it into float , then print it.
+ans= n = int (input("enter the number:"))
+     f=float(n)
+     print("convert integer into float:",f)
+
+10. write a program to take price of three items (float) and total price.
+   Ans= item 1 = float(input ("enter the price of first item:")
+        item 2 = float(input ("enter the price of second item:")
+        item 3 = float(input ("enter the price of third item:")
+        total= item1 + item2 + item3
+        print ("Total price of all items:", total)
